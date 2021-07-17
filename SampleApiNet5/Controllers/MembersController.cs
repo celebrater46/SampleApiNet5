fe8e30fc8e5a10412b8ea91a1dc3eff174987270
hideru <jota.16.stigma.world@gmail.com>
2021-07-17 16:13:42 +0900

    first commit

---
+++ SampleApiNet5/Controllers/MembersController.cs
@@ -0,0 +1,27 @@
+﻿namespace SampleApiNet5.Controllers
+{
+    public class MembersController : ApiController
+    {
+        Member[] members = new Member[]
+        {
+            new Member { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Member { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Member { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public IEnumerable<Member> GetAllProducts()
+        {
+            return members;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var member = members.FirstOrDefault((p) => p.Id == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return Ok(member);
+        }
+    }
+}
---
+++ SampleApiNet5/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿namespace SampleApiNet5.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
