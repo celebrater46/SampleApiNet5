25f7cc265fadaeec7a59864ff6c523b71bd7979a
hideru <jota.16.stigma.world@gmail.com>
2021-07-17 16:18:45 +0900

    Copied all files

---
+++ SampleApiNet5/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace SampleApiNet5
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API の設定およびサービス
+
+            // Web API ルート
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
